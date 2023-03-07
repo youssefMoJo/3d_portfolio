fa96429fc4bec75a9be5db45f8080de8adeb8ddf
youssefMoJo <youssefrafat@yahoo.com>
2023-03-06 22:30:58 -0330

    imported all the components of the portfolio in the App file

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,27 @@ import {About, Contact, Experience, Feedbacks, Hero,
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <div className="relative z-0 bg-primary">
         
+        {/* The headers of the portfolio */}
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+
+        {/* The body of the portfolio */}
+        <About/>
+        <Experience/>
+        <Tech/> 
+        <Works/>
+        <Feedbacks/>
+
+        {/* this section is for the stars */}
+        <div className="relative z-0">
+          <Contact/> 
+          <StarsCanvas/> 
+        </div>
+
       </div>
     </BrowserRouter>
   )
